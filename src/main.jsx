1dbad0b428118d4bad3e0f9adc9d2de106c48485
hyunsubLee <akmei90@gmail.com>
2025-07-23 17:08:53 +0900

    deploy 3

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter basename={import.meta.env.BASE_URL}>
