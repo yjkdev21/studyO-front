c52c505ddb0855330c76b0bcedc99de5bb44c4af
hyunsubLee <akmei90@gmail.com>
2025-07-23 17:49:32 +0900

    deploy start

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // BrowserRouter 대신 HashRouter 임포트
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename={import.meta.env.BASE_URL}>
+  // BrowserRouter 대신 HashRouter를 사용하고, basename 속성은 HashRouter에서 필요 없습니다.
+  <HashRouter>
     <StrictMode>
       <App />
     </StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 );
