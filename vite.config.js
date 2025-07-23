2dc15272d0f121ba815d720e95c1cc35f2930229
hyunsubLee <akmei90@gmail.com>
2025-07-23 17:38:58 +0900

    deploy---

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/react-study-o/", // <YOUR_REPO_NAME> 부분에 해당하는 경로
+  base: "/react-study-o/",
 });
