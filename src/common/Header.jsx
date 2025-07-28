9182f7aa2fe93d3f78c4e785cd58476b583028a4
yjkdev21 <yjkdev21@gmail.com>
2025-07-28 12:39:59 +0900

    Header Link Edit

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -1,13 +1,13 @@
-import { Border } from "react-bootstrap-icons";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 export default function Header() {
   return (
     <header style={{border: "1px solid", padding:"10px"}}>
-      <a href="/" style={{border: "1px solid"}}>
+      <Link to="/" style={{border: "1px solid"}}>
         <img src="../assets/logo.svg" alt="logo" />홈페이지
-      </a>
-      <a href="./join" style={{border: "1px solid"}}>회원가입</a>
-      <a href="./login" style={{border: "1px solid"}}>로그인</a>
+      </Link>
+      <Link to="/join" style={{border: "1px solid"}}>회원가입</Link>
+      <Link to="/login"style={{border: "1px solid"}}>로그인</Link>
     </header>
   );
 }
