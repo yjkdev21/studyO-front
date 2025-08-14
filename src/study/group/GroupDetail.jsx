482e708ce0d0c25ec745f962387d512fba47e60a
yuseon4455 <yuseon0344@naver.com>
2025-08-14 12:16:51 +0900

    홍보글 관리 링크 추가

--- src/study/group/GroupDetail.jsx
+++ src/study/group/GroupDetail.jsx
@@ -229,6 +229,9 @@ export default function GroupDetail() {
                 <Link to={`/study/${groupId}/dashboard`} className="btn btn-secondary">
                     대시보드
                 </Link>
+                <Link to="/study/postMain" className="btn btn-secondary">
+                    홍보글 관리
+                </Link>
                 {isOwner() && (
                     <>
                         <Link to={`/groupUpdate/${group.groupId}`} className="btn btn-primary">
