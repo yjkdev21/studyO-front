7e93bb342634812565eecefd1de60bbe8108b4f8
hyunsubLee <akmei90@gmail.com>
2025-08-20 00:09:45 +0900

    bbbb

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,6 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter basename={import.meta.env.BASE_URL}>
-    <StrictMode>
-      <App />
-    </StrictMode>
+    <App />
   </BrowserRouter>
 );
