e44f4a1a4fc5680f67ae46bb7a3c3583ab91055c
hyunsubLee <akmei90@gmail.com>
2025-08-20 00:54:01 +0900

    aaa

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
+  // strict 제거..
   <BrowserRouter basename={import.meta.env.BASE_URL}>
     <App />
   </BrowserRouter>
